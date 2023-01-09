8ee17f441de70c40d130534181b7ad6d3032c1e0
danielcrubens <dcr.tecnologi@gmail.com>
2023-01-09 19:25:59 -0300

    deploy13

--- src/app/models/File.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const Base = require('./Base');
-
-Base.init({table: 'files'});
-
-module.exports = {
-    ...Base
-};
--- src/app/models/RecipeFile.js
+++
@@ -1,7 +0,0 @@
-const Base = require('./Base');
-
-Base.init({table: 'recipe_files'});
-
-module.exports = {
-    ...Base
-};
